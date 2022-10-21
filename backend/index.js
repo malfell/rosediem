a516fb9f9798974fd01fce6fa6580662af504f1b
Mal F <malfellows13@gmail.com>
2022-10-20 19:50:55 -0500

    index.js adjustments and 404 route stubbed out

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 // Modules and Globals
+// require dotenv
+require('dotenv').config()
 // require express
 const express = require('express');
+
 
 // initialize the app after requiring express
 const app = express()
@@ -10,7 +13,12 @@ app.get('/', (req, res) => {
     res.send('Rosediem Art Duels')
 })
 
+// 404 Stub
+app.get('*', (req, res) => {
+    res.status(404).send('404 not found')
+})
+
 // listen for connection
-app.listen(3000, function(){
-    console.log('sup')
+app.listen(process.env.PORT, () => {
+    console.log(`sup from ${process.env.PORT}`)
 })
