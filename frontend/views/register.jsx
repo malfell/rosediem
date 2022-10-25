22922bcbeae166e796dd00f29fe16e069a7908b9
Mal F <malfellows13@gmail.com>
2022-10-25 14:51:26 -0500

    styled registration page some more

--- frontend/views/register.jsx
+++ frontend/views/register.jsx
@@ -12,36 +12,42 @@ function register(){
                         {/* USERNAME */}
                         <div className='form-group'>
                             <label htmlFor='email'>Username</label>
-                            <input className='form-control' type='text' placeholder='Username' id='username' name='username' required />
+                            <span aria-hidden="true" className="required-field">*</span>
+                            <input className='form-control' type='text' placeholder='Username' id='username' name='username' required aria-describedby="required-description" />
                         </div>
                         {/* EMAIL */}
                         <div className='form-group'>
                             <label htmlFor='email'>Email Address</label>
-                            <input className='form-control' type='text' placeholder='Email' id='email' name='email' required />
+                            <span aria-hidden="true" className="required-field">*</span>
+                            <input className='form-control' type='text' placeholder='Email' id='email' name='email' required aria-describedby="required-description"/>
                         </div>
                         {/* CONFIRM EMAIL */}
                         <div className='form-group'>
                             <label htmlFor='emailConfirm'>Confirm Email Address</label>
-                            <input className='form-control' type='text' placeholder='Confirm Email' id='emailConfirm' name='emailConfirm' required />
+                            <span aria-hidden="true" className="required-field">*</span>
+                            <input className='form-control' type='text' placeholder='Confirm Email' id='emailConfirm' name='emailConfirm' required aria-describedby="required-description"/>
                         </div>
                         {/* PASSWORD */}
                         <div className='form-group'>
                             <label htmlFor='password'>Password</label>
-                            <input className='form-control' type='password' placeholder='Password' id='password' name='password' required />
+                            <span aria-hidden="true" className="required-field">*</span>
+                            <input className='form-control' type='password' placeholder='Password' id='password' name='password' required aria-describedby="required-description"/>
                         </div>
                         {/* CONFIRM PASSWORD */}
                         <div className='form-group'>
                             <label htmlFor='passwordConfirm'>Confirm Password</label>
-                            <input className='form-control' type='password' placeholder='Confirm Password' id='passwordConfirm' name='passwordConfirm' required />
+                            <span aria-hidden="true" className="required-field">*</span>
+                            <input className='form-control' type='password' placeholder='Confirm Password' id='passwordConfirm' name='passwordConfirm' required aria-describedby="required-description"/>
                         </div>
                         {/* BIRTHDATE */}
                         <div className='form-group'>
                             <label htmlFor='passwordConfirm'>Birthday</label>
-                            <input className='form-control' type='date' placeholder='mm/dd/yyyy' id='birthday' name='birthday' required />
+                            <span aria-hidden="true" className="required-field">*</span>
+                            <input className='form-control' type='date' placeholder='mm/dd/yyyy' id='birthday' name='birthday' required aria-describedby="required-description"/>
                             <p style={{color: 'gray', fontWeight: 'normal'}}>Your birthday will not be public.</p>
                         </div>
                         {/* SUBMIT BUTTON */}
-                        <input type='submit' value='Register'/>                        
+                        <input type='submit' className='submit-button' value='Register'/>                        
                     </container>
                     
                 </form>
